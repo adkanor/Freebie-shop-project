d2407075cdff265c6141c7df6b18148b3153e25e
SerhiiBondarenko0693 <125159575+SerhiiBondarenko0693@users.noreply.github.com>
2023-09-25 00:10:41 +0300

    No page (#10)
    * adaptive no page
    
    * adaptive no page
    
    * adaptive no page
    
    * adaptive noPage
    
    * adaptive noPage

--- src/pages/NoPage/NoPage.jsx
+++ src/pages/NoPage/NoPage.jsx
@@ -5,10 +5,11 @@ import { useNavigate } from "react-router-dom";
 
 const NoPage = () => {
     const navigate = useNavigate();
-    return <div className={style.noPageWrapper}>
 
-        <div>404 not found</div>
-        <button onClick={() => navigate(-1)}>Go back</button>
+    return <div className = {`section ${ style.noPageWrapper }`}>
+        <div className={style.noPageTitle}>404 not found</div>
+        <p className={style.noPageSubtitle}>Your visited page not found. You may go home page.</p>
+        <button className={style.goBackBtn} onClick={() => navigate(-1)}>Go back</button>
     </div>;
 
 };
