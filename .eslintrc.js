9141cd35cbf7992a3e27b91ba7d482c8f79403c7
SerhiiBondarenko0693 <125159575+SerhiiBondarenko0693@users.noreply.github.com>
2023-09-24 15:28:50 +0300

    Fixed eslint+prettier (#7)
    * fixed eslint and pritier
    
    * fixed eslint and pritier
    
    * fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,44 +1,44 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-    jest: true,
-  },
-  extends: [
-    "react-app",
-    "react-app/jest",
-    "eslint:recommended",
-    "plugin:react/recommended",
-  ],
-  overrides: [
-    {
-      env: {
+    env: {
+        browser: true,
+        es2021: true,
         node: true,
         jest: true,
-      },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: {
-        sourceType: "script",
-      },
     },
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    extends: [
+        "react-app",
+        "react-app/jest",
+        "eslint:recommended",
+        "plugin:react/recommended",
+    ],
+    overrides: [
+        {
+            env: {
+                node: true,
+                jest: true,
+            },
+            files: [".eslintrc.{js,cjs}"],
+            parserOptions: {
+                sourceType: "script",
+            },
+        },
+    ],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: "latest",
+        sourceType: "module",
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["react"],
-  settings: {
-    react: {
-      version: "detect", //надо потом указать весрию
+    plugins: ["react"],
+    settings: {
+        react: {
+            version: "detect", //надо потом указать весрию
+        },
     },
-  },
-  rules: {
-    indent: ["error", 2],
-    quotes: ["error", "double"],
-    semi: ["error", "always"],
-  },
+    rules: {
+        indent: ["error", 4],
+        quotes: ["error", "double"],
+        semi: ["error", "always"],
+    },
 };
--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -3,19 +3,22 @@ import { Link } from "react-router-dom";
 import Separator from "../../components/Separator/Separator.jsx";
 // import styles from "./CartPage.module.css";
 const CartPage = () => {
-  return <>
-    <Separator/>
-    <div className="section">
-      <nav className="beadcrumbs">
-        <ul className="breadcrumbs-list">
-          <li className="breadcrumb-item"> <Link to="/" className="breadcrumb-link">Home</Link></li>
-          <li className="breadcrumb-item"> <Link to="/cart" className="breadcrumb-link">Cart</Link></li>
-        </ul>
-      </nav>
-    </div>
-  
-  
-  </>;
+
+
+    return <>
+        <Separator />
+        <div className="section">
+            <nav className="beadcrumbs">
+                <ul className="breadcrumbs-list">
+                    <li className="breadcrumb-item"><Link to="/" className="breadcrumb-link">Home</Link></li>
+                    <li className="breadcrumb-item"><Link to="/cart" className="breadcrumb-link">Cart</Link></li>
+                </ul>
+            </nav>
+        </div>
+
+
+    </>;
+
 };
 
 export default CartPage;
