7106ad24c05edfa80686f3d14e49fe236cfd4398
Serhii Bondarenko <sergiy.ol.bondarenko@gmail.com>
2023-09-24 15:13:17 +0300

    fix

--- src/components/Separator/Separator.jsx
+++ src/components/Separator/Separator.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import styles from "./Separator.module.css";
-const Separator = ()=>{
-  return <>
-    <div className={styles.separator}></div>
-  </>;
+
+const Separator = () => {
+    return <>
+        <div className={styles.separator}></div>
+    </>;
 };
 export default Separator;
