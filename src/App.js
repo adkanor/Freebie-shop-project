f129bf4300203525585273182543e5490e7b9c48
JevgenBalashov <130686674+JevgenBalashov@users.noreply.github.com>
2023-09-25 22:58:17 +0300

    task5 initial commit (#11)
    * task5 initial commit
    
    * new app.js
    
    * commit
    
    * new updates

--- src/App.js
+++ src/App.js
@@ -13,9 +13,10 @@ const App = () => {
             <Routes>
                 <Route path="/" element={<MainSection />} />
                 <Route path="/cart" element={<CartPage />} />
-                <Route path="/footer" element={<Footer />} />
+                {/* <Route path="/footer" element={<Footer />} /> */}
                 <Route path="*" element={<NoPage />} />
             </Routes>
+            <Footer />
         </>
     );
 };
