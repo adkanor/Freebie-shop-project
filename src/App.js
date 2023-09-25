6041e55ab814b2d5accfe421a5cdc41c4404b0ba
Jevgen Balashov <jvgnbal@gmail.com>
2023-09-25 14:17:14 +0300

    new app.js

--- src/App.js
+++ src/App.js
@@ -7,19 +7,6 @@ import MainSection from "./pages/MainPage/MainPage";
 import NoPage from "./pages/NoPage/NoPage";
 
 const App = () => {
-<<<<<<< Updated upstream
-    return (
-        <>
-            <Header />
-            <Routes>
-                <Route path="/" element={<MainSection />} />
-                <Route path="/cart" element={<CartPage />} />
-                <Route path="/footer" element={<Footer />} />
-                <Route path="*" element={<NoPage />} />
-            </Routes>
-        </>
-    );
-=======
   return (
     <>
       <Header />
@@ -32,7 +19,6 @@ const App = () => {
       <Footer />
     </>
   );
->>>>>>> Stashed changes
 };
 
 export default App;
