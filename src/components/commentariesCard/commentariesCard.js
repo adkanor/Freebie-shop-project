6d6d3110ce5838b97ba869b976fb7830bb7a3c89
Serhii Bondarenko <sergiy.ol.bondarenko@gmail.com>
2023-09-29 23:29:11 +0300

    add card comments

---
+++ src/components/commentariesCard/commentariesCard.js
@@ -0,0 +1,29 @@
+import React from "react";
+import approvedIcon from "../../assets/icons/approvedUserIcon/approvedUserIcon.svg";
+import style from "./commentariesCard.module.css";
+import StarRating from "../StarRating/StarRating";
+import PropTypes from "prop-types";
+
+
+const CommentariesCard = ({name, commentaries, rating}) => {
+    return (
+        <div className={style.commentariesWrapper}>
+            <StarRating rating={rating} starSize={"23px"}/>
+            <div className={style.commentariesUserInfo} >
+                <p className={style.commentariesUserName}>{name}</p>
+                <img src={approvedIcon} alt={"approvedIcon"} />
+            </div>
+            <p className={style.commentariesText}>{commentaries}</p>
+            
+        </div>
+    );
+};
+
+CommentariesCard.propTypes = {
+    name:PropTypes.string.isRequired,
+    commentaries:PropTypes.string.isRequired,
+    rating:PropTypes.number.isRequired
+    
+};
+
+export default CommentariesCard;
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./MainPage.module.css";
 import { Link } from "react-router-dom";
 import Slider from "../../components/Slider/Slider";
+import CommentariesCard from "../../components/commentariesCard/commentariesCard";
 const MainPage = () => {
     return (
         <section className="section">
@@ -23,6 +24,7 @@ const MainPage = () => {
                     </Link>
                 </div>
             </div>
+            <CommentariesCard commentaries={"\"As someone who's always on the lookout for unique fashion pieces, I'm thrilled to have stumbled upon Shop.co. The selection of clothes is not only diverse but also on-point with the latest trends.”"} name={"user"} rating={4.3}/>
         </section>
     );
 
