1d8ca2923df2af023c9b8e417640b7903dc68b86
Serhii Bondarenko <sergiy.ol.bondarenko@gmail.com>
2023-10-09 19:40:20 +0300

    add google auth

--- src/index.js
+++ src/index.js
@@ -2,18 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import store from "./stores/store";
-import { Provider } from "react-redux";
+import {Provider} from "react-redux";
+import {GoogleOAuthProvider} from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <>
         <Provider store={store}>
             <BrowserRouter>
-                <App />
+                <GoogleOAuthProvider
+                    clientId={"1067707080438-8h0es3omklr0q43jc6435pf331576ftp.apps.googleusercontent.com"}>
+                    <App/>
+                </GoogleOAuthProvider>
             </BrowserRouter>
         </Provider>,
     </>
-    
 );
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,16 +1,40 @@
 import React from "react";
 import style from "./Login.module.css";
-import { Form, Formik } from "formik";
+import jwt_decode from "jwt-decode";
+import {Form, Formik} from "formik";
 import Input from "../../components/InputPassworgLogin/Input";
 import validationSchema from "./validationSchema";
 import Button from "../../components/Button/Button";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
+import {GoogleLogin} from "@react-oauth/google";
 
 
-const url ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQz6RwvgggYzVqj1g1WegTjmGl5vzxPaCm6Lg&usqp=CAU";
+const url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQz6RwvgggYzVqj1g1WegTjmGl5vzxPaCm6Lg&usqp=CAU";
 
 
 const Login = () => {
+
+
+    // const sendPostMessage = (data) => {
+    //     const targetWindow = window.parent;
+    //     targetWindow.postMessage(data, "http://localhost:3000/login");
+    // };
+
+    // useEffect(() => {
+    //     const receiveMessage = (event) => {
+    //         if (event.origin === "http://localhost:3000/login") {
+    //             console.log("Отримано повідомлення:", event.data);
+    //         }
+    //     };
+    //
+    //     window.addEventListener("message", receiveMessage);
+    //
+    //     return () => {
+    //         window.removeEventListener("message", receiveMessage);
+    //     };
+    // }, []);
+
+
     return (
         <div className={`section ${style.loginContainer}`}>
             <div className={style.bannerContainer}>
@@ -26,47 +50,67 @@ const Login = () => {
                             password: ""
                         }}
                         validationSchema={validationSchema}
-                        onSubmit= {(values) => {
+                        onSubmit={(values) => {
                             console.log("++++");
                             console.log(values, "FormValue");
                         }}
                     >
-                        {({ errors, touched }) => (
+                        {({errors, touched}) => (
                             <Form className={style.formWrapper}>
                                 <Input
                                     name="email"
                                     placeholder="Email or Phone Number"
-                                    isError={errors.email && touched.email }
+                                    isError={errors.email && touched.email}
                                     errorText={errors.email}
                                     type={"email"}
                                 />
                                 <Input
                                     name="password"
                                     placeholder="Password"
-                                    isError={errors.password && touched.password }
+                                    isError={errors.password && touched.password}
                                     errorText={errors.password}
                                     type={"password"}
                                 />
                                 <div className={style.loginBtn}>
                                     <Button type={"submit"} text={"Log In"} style={{
                                         padding: "16px 35px",
-                                        fontSize:"16px",
+                                        fontSize: "16px",
                                         backgroundColor: "var(--login-btn)",
                                         color: "var(--white-text)",
                                         border: "none",
                                     }}/>
-                                    <Link className={style.ForgetPassword} to="/registration" > Forget Password?</Link>
+                                    <Link className={style.ForgetPassword} to="/registration"> Forget Password?</Link>
                                 </div>
-
 
                             </Form>
                         )}
+
                     </Formik>
+                </div>
+                <div>
+                    <GoogleLogin
+                        onSuccess={credentialResponse => {
+                            const decoded = jwt_decode(credentialResponse.credential);
+                            console.log(decoded);
+
+
+                        }}
+                        onError={() => {
+                            console.log("Login Failed");
+                        }}
+                    />
                 </div>
 
             </div>
+
+
         </div>
     );
 };
 
 export default Login;
+
+
+
+
+
