638d459b4a67d6fdb104c5978d295605a1317e2a
adkanor <125124730+adkanor@users.noreply.github.com>
2023-10-11 18:41:13 +0200

    Cart functionality (#68)
    * adding and delete from cart functionality
    
    * cart increase and decrease functionality

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,14 +1,14 @@
-import {configureStore} from "@reduxjs/toolkit";
-import {combineReducers} from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
 import newArrivalsReducer from "./newArrivals/newArrivalsReducer";
 import topSaleReducer from "./topSelling/topSellingReducer";
-import cartReducer from "./cartReducer";
-
+import cartReducer from "./cartProducts/cartReducer";
 const rootReducer = combineReducers({
-    newArrivalsReducer, topSaleReducer, cartReducer
-
+    newArrivalsReducer,
+    topSaleReducer,
+    cartReducer,
 });
 
 const store = configureStore({
