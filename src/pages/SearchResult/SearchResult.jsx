f479907028ca4d2a11ba3412c55d03288fd86272
Misha Sysoev <54980483+Lamass566@users.noreply.github.com>
2023-10-18 13:55:22 +0300

    fix search

--- src/pages/SearchResult/SearchResult.jsx
+++ src/pages/SearchResult/SearchResult.jsx
@@ -8,21 +8,26 @@ import { useParams } from "react-router-dom";
 import RenderComponent from "./RenderComponent/RenderComponent";
 
 const SearchResult = () => {
+    const { value } = useParams();
     const dispatch = useDispatch();
     let allProducts = useSelector(
         (state) => state.searchProductsReducer.filteredData
     );
     let allArrivals = useSelector((state) => state.newArrivalsReducer);
     let allTopSelling = useSelector((state) => state.topSaleReducer);
-    const { value } = useParams();
 
     useEffect(() => {
-        dispatch(getAllProducts());
-    }, [dispatch]);
+        const fetchData = async () => {
+            if (allProducts.length > 0) {
+                await dispatch(getFilteredProducts(value));
+            } else {
+                await dispatch(getAllProducts());
+                await dispatch(getFilteredProducts(value));
+            }
+        };
 
-    useEffect(() => {
-        dispatch(getFilteredProducts(value));
-    }, [value, dispatch]);
+        fetchData();
+    }, [value, dispatch, allProducts.length]);
 
     return (
         <div className="section">
