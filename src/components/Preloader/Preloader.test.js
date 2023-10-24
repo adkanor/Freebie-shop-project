28742506581a178ba09d496bda1d013f92e6919e
Jevgen Balashov <jvgnbal@gmail.com>
2023-10-24 19:01:39 +0300

    fix bug

--- src/components/Preloader/Preloader.test.js
+++ src/components/Preloader/Preloader.test.js
@@ -4,11 +4,6 @@ import "@testing-library/jest-dom/extend-expect";
 import Preloader from "./Preloader";
 
 test("Preloader renders without errors", () => {
-<<<<<<< HEAD
   const { container } = render(<Preloader />);
   expect(container).toBeInTheDocument();
-=======
-    const { container } = render(<Preloader />);
-    expect(container).toBeInTheDocument();
->>>>>>> development
 });
