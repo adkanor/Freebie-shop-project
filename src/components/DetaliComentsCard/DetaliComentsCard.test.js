f53b302ada8939c940bb1d2f9d8369872bcdfe9c
Serhii Bondarenko <sergiy.ol.bondarenko@gmail.com>
2023-10-26 15:51:59 +0300

    add test

--- src/components/DetaliComentsCard/DetaliComentsCard.test.js
+++ src/components/DetaliComentsCard/DetaliComentsCard.test.js
@@ -3,19 +3,50 @@ import {render, waitFor} from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import axios from "axios";
 import DetaiLComentsCard from "./DetaliComentsCard";
+import MockAdapter from "axios-mock-adapter";
 
 jest.mock("axios");
 
 describe("DetaiLComentsCard", () => {
     it("Axios get test", async () => {
-        axios.get.mockRejectedValueOnce(new Error("404 Not Found"));
-        console.error = jest.fn();
+        const mockComments = [
+            {
+                _id: 1,
+                firstName: "John",
+                lastName: "Doe",
+                text: "First comment",
+                rating: 5,
+            },
+            {
+                _id: 2,
+                firstName: "Jane",
+                lastName: "Doe",
+                text: "Second comment",
+                rating: 4,
+            },
+        ];
+        const setComments = jest.fn();
 
-        render(<DetaiLComentsCard idGoods="someId"/>);
+        jest.spyOn(React, "useState").mockReturnValue([mockComments, setComments]);
+
+        const idGoods = 123;
+        const mock = new MockAdapter(axios);
+        mock
+            .onGet(
+                "https://shopcoserver-git-main-chesterfalmen.vercel.app/api/comments/"
+            )
+            .reply(200, mockComments);
+
+        axios.get(
+            "https://shopcoserver-git-main-chesterfalmen.vercel.app/api/comments"
+        ).then(function (response) {
+            console.log(response.data);
+        });
+
+        render(<DetaiLComentsCard idGoods={idGoods}/>);
+
         await waitFor(() => {
-            expect(console.error).toHaveBeenCalledWith("404", new Error("404 Not Found"));
+            expect(setComments).toHaveBeenCalledWith(mockComments);
         });
     });
-
-
 });
