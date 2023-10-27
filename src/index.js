07a66b5972ff6e87d9964baba07d41ff88c622e6
Kateryna Mordovtseva <katya162157@gmail.com>
2023-10-27 17:52:36 +0200

    add config file to deploy project

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import store from "./stores/store";
-import {Provider} from "react-redux";
-import {GoogleOAuthProvider} from "@react-oauth/google";
-
+import { Provider } from "react-redux";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { GMAILID } from "../config";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <>
@@ -14,8 +14,9 @@ root.render(
             <BrowserRouter>
                 <GoogleOAuthProvider
                     script-src={"https://accounts.google.com/gsi/"}
-                    clientId={"260867558541-ctmfm0db7e62mj2dlm4881mp8th6g1cc.apps.googleusercontent.com"}>
-                    <App/>
+                    clientId={GMAILID}
+                >
+                    <App />
                 </GoogleOAuthProvider>
             </BrowserRouter>
         </Provider>
