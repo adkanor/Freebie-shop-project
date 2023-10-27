9e50bb06e46d2d117899271dd5bc25f16b8c0c81
Serhii Bondarenko <sergiy.ol.bondarenko@gmail.com>
2023-10-27 15:08:46 +0300

    add test

--- src/components/DetaliComentsCard/DetaliComentsCard.test.js
+++ src/components/DetaliComentsCard/DetaliComentsCard.test.js
@@ -3,50 +3,19 @@ import {render, waitFor} from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import axios from "axios";
 import DetaiLComentsCard from "./DetaliComentsCard";
-import MockAdapter from "axios-mock-adapter";
 
 jest.mock("axios");
 
 describe("DetaiLComentsCard", () => {
     it("Axios get test", async () => {
-        const mockComments = [
-            {
-                _id: 1,
-                firstName: "John",
-                lastName: "Doe",
-                text: "First comment",
-                rating: 5,
-            },
-            {
-                _id: 2,
-                firstName: "Jane",
-                lastName: "Doe",
-                text: "Second comment",
-                rating: 4,
-            },
-        ];
-        const setComments = jest.fn();
+        axios.get.mockRejectedValueOnce(new Error("404 Not Found"));
+        console.error = jest.fn();
 
-        jest.spyOn(React, "useState").mockReturnValue([mockComments, setComments]);
-
-        const idGoods = 123;
-        const mock = new MockAdapter(axios);
-        mock
-            .onGet(
-                "https://shopcoserver-git-main-chesterfalmen.vercel.app/api/comments/"
-            )
-            .reply(200, mockComments);
-
-        axios.get(
-            "https://shopcoserver-git-main-chesterfalmen.vercel.app/api/comments"
-        ).then(function (response) {
-            console.log(response.data);
-        });
-
-        render(<DetaiLComentsCard idGoods={idGoods}/>);
-
+        render(<DetaiLComentsCard idGoods="someId"/>);
         await waitFor(() => {
-            expect(setComments).toHaveBeenCalledWith(mockComments);
+            expect(console.error).toHaveBeenCalledWith("404", new Error("404 Not Found"));
         });
     });
+
+
 });
