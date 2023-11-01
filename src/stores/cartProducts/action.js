52559c212602c3aa6f748b9aad104cee54e63e54
adkanor <125124730+adkanor@users.noreply.github.com>
2023-11-01 18:32:09 +0100

    change logic for cart(made auth validation) (#142)

--- src/stores/cartProducts/action.js
+++ src/stores/cartProducts/action.js
@@ -3,6 +3,13 @@ export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
 export const INCREMENT_ITEM_QUANTITY = "INCREMENT_ITEM_QUANTITY";
 export const DECREMENT_ITEM_QUANTITY = "DECREMENT_ITEM_QUANTITY";
 export const CLEAR_CART = "CLEAR_CART";
+export const REFRESH_CART = "REFRESH_CART";
+
+export const refreshCart = (array) => ({
+    type: REFRESH_CART,
+    payload: array,
+});
+
 export const addToCart = (item) => ({
     type: ADD_TO_CART,
     payload: item,
