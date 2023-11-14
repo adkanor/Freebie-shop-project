1c0cbef59b78457160b6670f58d5e80d7886708b
Jevgen Balashov <jvgnbal@gmail.com>
2023-11-14 17:02:22 +0200

    fix test for footer

--- src/components/Footer/Footer.test.js
+++ src/components/Footer/Footer.test.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+
 import "@testing-library/jest-dom/extend-expect";
 
 import Footer from "./Footer";
 
 test("checks if the Footer component renders without errors", () => {
-  render(<Footer />);
+  render(
+    <BrowserRouter>
+      <Footer />
+    </BrowserRouter>
+  );
   expect(screen.getByText("shop.co")).toBeInTheDocument();
 });
 
 test('checks the presence of social media icons', () => {
-  render(<Footer />);
+  render(
+    <BrowserRouter>
+      <Footer />
+    </BrowserRouter>
+  );
   expect(screen.getByAltText("TwitterIcon")).toBeInTheDocument();
   expect(screen.getByAltText("FaceBookIcon")).toBeInTheDocument();
   expect(screen.getByAltText("InstagramIcon")).toBeInTheDocument();
@@ -18,15 +28,18 @@ test('checks the presence of social media icons', () => {
 });
 
 test("checks the presence and functionality of the email subscription form", () => {
-    render(<Footer />);
-  
-    const emailContainerTitle = screen.getByText("Stay Up to Date About Our Latest Offers");
-    expect(emailContainerTitle).toBeInTheDocument();
-  
-    const emailInput = screen.getByPlaceholderText("Enter your email address");
-    expect(emailInput).toBeInTheDocument();
-  
-    const subscribeButton = screen.getByText("Subscribe to newsletter");
-    expect(subscribeButton).toBeInTheDocument();
-    
-  });
+  render(
+    <BrowserRouter>
+      <Footer />
+    </BrowserRouter>
+  );
+
+  const emailContainerTitle = screen.getByText("Stay Up to Date About Our Latest Offers");
+  expect(emailContainerTitle).toBeInTheDocument();
+
+  const emailInput = screen.getByPlaceholderText("Enter your email address");
+  expect(emailInput).toBeInTheDocument();
+
+  const subscribeButton = screen.getByText("Subscribe to newsletter");
+  expect(subscribeButton).toBeInTheDocument();
+});
