3a4436929c303104b3bb337273bef12f3b0ce64d
Kateryna Mordovtseva <katya162157@gmail.com>
2023-11-16 22:45:05 +0100

    add links to Nav and remove &from the end of def

--- src/components/CommentsCard/CommentsCard.jsx
+++ src/components/CommentsCard/CommentsCard.jsx
@@ -4,35 +4,39 @@ import styles from "./CommentsCard.module.css";
 import StarRating from "../StarRating/StarRating";
 import PropTypes from "prop-types";
 
-
-const CommentsCard = ({firstName, lastName, commentaries, rating, style, className}) => {
+const CommentsCard = ({
+    firstName,
+    commentaries,
+    rating,
+    style,
+    className,
+}) => {
     const cardStyle = {
         ...style,
-
     };
-    const author = firstName + " " + lastName.charAt(0) + ".";
+    const author = firstName;
     return (
-        <div style={cardStyle} className={`${styles.commentsWrapper} ${className}`}>
-            <StarRating rating={parseInt(rating)} starSize={"23px"}/>
+        <div
+            style={cardStyle}
+            className={`${styles.commentsWrapper} ${className}`}
+        >
+            <StarRating rating={parseInt(rating)} starSize={"23px"} />
             <div className={styles.commentsUserInfo}>
                 <p className={styles.commentsUserName}>{author}</p>
-                <img src={approvedIcon} alt={"approvedIcon"}/>
+                <img src={approvedIcon} alt={"approvedIcon"} />
             </div>
 
             <p className={styles.commentsText}>{commentaries}</p>
-
         </div>
     );
 };
 
 CommentsCard.propTypes = {
     firstName: PropTypes.string.isRequired,
-    lastName: PropTypes.string.isRequired,
     commentaries: PropTypes.string.isRequired,
     rating: PropTypes.number.isRequired,
     style: PropTypes.object,
-    className: PropTypes.string
-
+    className: PropTypes.string,
 };
 
 export default CommentsCard;
