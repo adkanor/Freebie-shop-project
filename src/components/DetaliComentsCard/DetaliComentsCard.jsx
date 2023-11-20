2d5d52137b383a0af173dd8c7092acfd7c5b94d6
Serhii Bondarenko <sergiy.ol.bondarenko@gmail.com>
2023-11-20 10:35:33 +0200

    add show pass

--- src/components/DetaliComentsCard/DetaliComentsCard.jsx
+++ src/components/DetaliComentsCard/DetaliComentsCard.jsx
@@ -18,7 +18,6 @@ const DetaiLComentsCard = ({ details, idGoods, FAQ }) => {
         axios
             .get(`${URL}getcomments/?page=1&limit=10&sort=new&good=${idGoods}`)
             .then((res) => {
-                res.data.sort((a, b) => b.rating - a.rating);
                 setComments(res.data);
             })
             .catch((error) => {
